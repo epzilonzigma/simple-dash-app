01a50ee58be5bec70966115739406984ae0e00c3
Tony Cai <tony.zx.cai@gmail.com>
2021-08-12 10:32:48 +0000

    return forecast

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import statsmodels.api as sm
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
 
 import pickle
 from utils.feature_calculation import *
@@ -12,7 +13,7 @@ app = dash.Dash(__name__)
 
 forecasted_units = 0
 
-app.layout = html.Div(children = [
+app.layout = html.Div(children = [ #setup app layout
     html.Div(# header
         children = [
             html.H1(
@@ -92,20 +93,42 @@ app.layout = html.Div(children = [
     html.Div(# calculate button + result
         children =[
             html.Button(
-                children = 'Estimate Order Quanity'
+                children = 'Estimate Order Quanity', 
+                id = 'forecast_button',
+                n_clicks = 0
             ),
             html.Br(),
-            html.Textarea(
-                children = f'{forecasted_units}',
-                style = {'marginTop': 14, 'fontSize': 20, 'textAlign': 'right', 'height': '24px', 'width': '10%'}
-            ),
-            html.Span(
-                children = 'units',
-                style = {'fontSize': 20, 'marginTop': 14}
+            html.P(
+                style = {'marginTop': 14, 'fontSize': 20, 'textAlign': 'left', 'height': '24px', 'width': '15%', 'background': 'lightBlue'},
+                id = 'forecast'
             )
         ]
     )
 ])
+
+# load model
+
+with open('./models/OLS.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# callback functions
+
+@app.callback(
+    Output(component_id = 'forecast', component_property = 'children'), #tells app where to spit the output
+    [Input('forecast_button', 'n_clicks')],
+    Input(component_id = 'year', component_property = 'value'),
+    Input(component_id = 'month', component_property = 'value'),
+    Input(component_id = 'product-type', component_property = 'value'),
+    Input(component_id = 'price', component_property = 'value'),
+    Input(component_id = 'product-characteristics', component_property = 'value'),
+    Input(component_id = 'umbrella-characteristics', component_property = 'value')
+)
+def generate_forecast(forecast_button, year, month, product_type, price, product_characteristics, umbrella_characteristics):
+    triggered_id = dash.callback_context.triggered[0]['prop_id'] #takes in the latest state variable to be updated
+    if triggered_id == 'forecast_button.n_clicks': #if the latest state variable to be updated is numbers clicked for the estimate button
+        input_df = construct_input_dataframe(year, month, product_type, price, product_characteristics, umbrella_characteristics)
+        forecast = predict_order_volume(input_df, model)
+        return f'{forecast} units'
 
 if __name__ == '__main__':
     app.run_server(port = 4000, debug = True, dev_tools_hot_reload = True)
--- utils/feature_calculation.py
+++ utils/feature_calculation.py
@@ -1,7 +1,44 @@
 import numpy as np
+import pandas as pd
 
 def sin_trans(x):
     return np.sin((x-1)*(2.*np.pi/12))
 
 def cos_trans(x):
     return np.cos((x-1)*(2.*np.pi/12))
+
+def construct_input_dataframe(year, month, product_type, price, product_characteristics, umbrella_characteristics):
+    df_month_sin = sin_trans(int(month))
+    df_month_cos = cos_trans(int(month))
+    kids = 0
+    dots = 0
+    umbrella = 0
+    folding = 0
+    ruffled = 0
+    hat = 0
+    poncho = 0
+
+
+    if 'child' in product_characteristics:
+        kids = 1
+    if 'dots' in product_characteristics:
+        dots = 1
+    if product_type == 'umbrella':
+        umbrella = 1
+    if product_type == 'hat':
+        hat = 1
+    if product_type == 'poncho':
+        poncho = 1
+    if 'ruffled' in umbrella_characteristics:
+        ruffled = 1
+    if 'folding' in umbrella_characteristics:
+        folding = 1
+        
+
+    df = pd.DataFrame({'Price': price, 'umbrella': umbrella, 'folding': folding, 'poncho': poncho, 'hat': hat, 'dots': dots, 'kids': kids, 'ruffled': ruffled, 'order_month_sin': df_month_sin,'order_month_cos': df_month_cos}, index = [0])
+
+    return df
+
+def predict_order_volume(input, model):
+    forecast = model.predict(input).iloc[0]
+    return forecast.round()
